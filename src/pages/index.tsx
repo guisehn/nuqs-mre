a496ce54b4b1b6f44daf7daab9de4b73dba640f0
Guilherme Sehn <guilherme.sehn@therealreal.com>
2025-12-23 11:19:48 -0300

    test fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import {
-  LoaderOptions, // <- deprecated
-  CreateLoaderOptions, // <- not exported
+  CreateLoaderOptions,
   parseAsArrayOf,
   parseAsString,
   ParserMap,
 } from 'nuqs';
-
 
 const QUERY_PARAMS: ParserMap = {
   clothing_size: parseAsArrayOf(parseAsString),
   shoe_size: parseAsArrayOf(parseAsString),
 };
 
-const QUERY_OPTIONS: LoaderOptions<typeof QUERY_PARAMS> = {
+const QUERY_OPTIONS: CreateLoaderOptions<typeof QUERY_PARAMS> = {
   urlKeys: {
     clothing_size: 'clothing_size[]',
     shoe_size: 'shoe_size[]',
